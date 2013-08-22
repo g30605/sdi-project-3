7eb20b73b77965b25ebb3e3a91499cba537befd2
Joel Adams <jadams924@fullsail.edu>
2013-08-22 18:46:48 -0400

    4

--- Adams_Joel_assignment3.js
+++ Adams_Joel_assignment3.js
@@ -11,19 +11,43 @@ var footballTeam = function (name) {
   // Accessors
   var getName = function () { return name; };
   var getCoach = function () { return coach; };
-  var getPlayer = function () { return player; };
-  var getRoster = function () {
-    for (var i = 0; j = roster.length; i < j; i++) {
-     console.log(roster[i] + "players on the team.");
-    };
-  };  
-  // Mutators
+  var getPlayer = function (player) {
+		for (var i =0, j = roster.length; i < j; i++){
+			if (roster[i] === player) {
+				console.log(player + " " + player.age(player) + " " + player.born(player));
+			} else {
+				console.log("Player not found.");
+			}
+		};
+		return player;
+	};
+	var getRoster = function () { return roster; };
+
+	// Mutators
+	var cutPlayer = function (player) {   // player takes an array of player or players
+		for (var i = 0, j = player.length; i < j; i++) {
+			if (player[i] === "") {
+				break;
+			} else {
+				var playerRemove = roster.indexOf(player[i]);
+				if (playerRemove === -1) {
+					console.log("Player not found.");
+				} else {
+					console.log(playerRemove);
+					roster.splice(playerRemove,1);
+				};
+			};
+		};
+	};
+	var addPlayer = function (player) {  // player takes an array of player or players
+		for (var i =0, j = player.length; i<j; i++) {
+			roster.push(player[i]);
+		};
+	};
 
 
-
-
-  // Public methods
-return {
+	// Public methods
+	return {
 		"teamName": getName,
 		"teamCoach": getCoach,
 		"teamPlayer": getPlayer,
@@ -37,9 +61,14 @@ return {
 var player = function (name, age, born) {
 	// Private
 	// Accessors
-	var getName = function () { return name;};
-	var getAge = function () { return age;};
-
+	var getName = function () { return name; };
+	var getAge = function (name) { return age; };
+	var bornInfo = function (name) {
+		var bornOutput = "Born in " + born[1] + ", " + born[2] + " on " + born[0] + ".";
+		return bornOutput;
+	};
+	var playerNumber = function () {};
+	var teamPlaysFor = function () {};
 	// Public methods
 	return {
 		"name": getName,
@@ -49,3 +78,11 @@ var player = function (name, age, born) {
 		"teamPlaysFor": teamPlaysFor
 	};
 };
+
+
+var blane = player("Blane lane", 32, ["3-24-81","Buffalo","New York"]);
+console.log(blane.age());
+console.log(blane.born());
+var team1 = footballTeam("Team 1");
+team1.addPlayer(["Blane lane", "Peter Tale", "Brian Simmons", "Carl Hunley", "James Water", "Tini Dome"]);
+console.log(team1.roster());
