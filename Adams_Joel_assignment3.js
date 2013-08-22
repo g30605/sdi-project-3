709cd186a53acc29e35f1ab910dc4db05ed22ddd
Joel Adams <jadams924@fullsail.edu>
2013-08-22 19:36:22 -0400

    6

--- Adams_Joel_assignment3.js
+++ Adams_Joel_assignment3.js
@@ -4,10 +4,10 @@ Date: 22 August 2013
 SDI Project 3 
 */
 
-var footballTeam = function (name) {
+var footballTeam = function (name, coach) {
   // Private
   var roster = [];
-  var coach, player;
+  var coach;
   // Accessors
   var getName = function () { return name; };
   var getCoach = function () { return coach; };
@@ -15,13 +15,15 @@ var footballTeam = function (name) {
 		for (var i =0, j = roster.length; i < j; i++){
 			if (roster[i] === player.name(player)) {
 				 var playerInfo = roster[i] + " " + "age: " + player.age() + "\n" + player.born();
-				 break;
-			} else {
-				console.log("Player not found.");
-			}
+				 
+				 			}
 		};
-		return playerInfo;
-	};
+	 if (playerInfo === undefined) {
+	 return playerInfo = "Player not on the roster.";
+	 } else {
+	  return playerInfo;
+	  };
+	   };
 	var getRoster = function () { return roster; };
 
 	// Mutators
@@ -81,13 +83,30 @@ var player = function (name, age, born, number, position) {
 };
 
 
-var blanelane = player("Blane lane", 32, ["3-24-81","Buffalo","New York"], 7, "Quarterback");
-console.log(blanelane.age());
-console.log(blanelane.born());
-var team1 = footballTeam("Team 1");
-team1.addPlayer(["Blane lane", "Peter Tale", "Brian Simmons", "Carl Hunley", "James Water", "Tini Dome"]);
+// Create players
+var player1 = player("Blane lane", 32, ["3-24-81","Buffalo","NY"], 7, "QB");
+var player2 = player("Peter Tale", 29, ["7-2-84","Dallas","TX"], 5, "OT");
+var player3 = player("Brian Simmons", 32, ["5-25-81","Richmond","VA"], 12, "RB");
+var player4 = player("Carl Hunley", 32, ["1-17-81","Grand Island","NY"], 2, "WR");
+var player5 = player("James Water", 29, ["2-23-84","Fort Erie","CA"], 15, "FB");
+var player6 = player("Tini Dome", 25, ["7-20-88","Cleveland","Ohio"], 23, "DB");
+// Create Team
+var bills = footballTeam("bills", "buffalo");
+// Add players to team
+bills.addPlayer(["Blane lane", "Peter Tale", "Brian Simmons", "Carl Hunley", "James Water", "Tini Dome"]);
 
-console.log(team1.roster());
-team1.cutPlayer(["Brian Simmons"]);
-console.log(team1.roster());
-console.log(team1.teamPlayer(blanelane));
+console.log(player1.age());
+console.log(player1.born());
+var bills = footballTeam("Team 1");
+bills.addPlayer(["Blane lane", "Peter Tale", "Brian Simmons", "Carl Hunley", "James Water", "Tini Dome"]);
+console.log(bills.roster());
+bills.cutPlayer(["Brian Simmons"]);
+console.log(bills.roster());
+console.log(player1.name());
+console.log(" ");
+console.log(bills.teamPlayer(player1));
+console.log(bills.teamPlayer(player2));
+console.log(bills.teamPlayer(player3)); // Cut from team should not show up on roster
+console.log(bills.teamPlayer(player4));
+console.log(bills.teamPlayer(player5));
+console.log(bills.teamPlayer(player6));
