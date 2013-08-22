d3578ef93eecda098430fb107119fdc9d4b594be
Joel Adams <jadams924@fullsail.edu>
2013-08-22 18:31:31 -0400

    2

--- Adams_Joel_assignment3.js
+++ Adams_Joel_assignment3.js
@@ -3,3 +3,33 @@ Author: Joel E. Adams
 Date: 22 August 2013
 SDI Project 3 
 */
+
+var footballTeam = function (name) {
+  // Private
+  var roster = [];
+  var coach, player;
+  // Accessors
+  var getName = function () { return name; };
+  var getCoach = function () { return coach; };
+  var getPlayer = function () { return player; };
+  var getRoster = function () {
+    for (var i = 0; j = roster.length; i < j; i++) {
+     console.log(roster[i] + "players on the team.");
+    };
+  };  
+  // Mutators
+
+
+
+
+  // Public methods
+  return {
+    "name": getName,
+    "coach": getCoach,
+    "player": getPlayer,
+    "roster": getRoster,
+    "cutPlayer": cutPlayer,
+    "addPlayer": addPlayer
+
+  };
+};
